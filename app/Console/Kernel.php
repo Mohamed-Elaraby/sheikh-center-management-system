9664c822f002d0b2ee6a45501fc71ece19f1e31b
Mohamed <mido.b333@gmail.com>
2024-01-02 11:27:22 +0400

    fix some bugs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,9 +28,9 @@ class Kernel extends ConsoleKernel
 //        $schedule->command('backup:clean')->everyMinute();
 //        $schedule->command('backup:run --only-db') ->everyMinute();
 //        $schedule -> command('moneySafe:getOpeningBalance')->dailyAt('10:00')->timezone('Asia/Dubai');
-        $schedule -> command('moneySafe:getOpeningBalance')->dailyAt('10:00')->timezone('Asia/Riyadh');
+//        $schedule -> command('moneySafe:getOpeningBalance')->dailyAt('10:00')->timezone('Asia/Riyadh');
 //        $schedule->command("moneySafe:getOpeningBalance")->daily()->at("10:39");
-//        $schedule -> command('moneySafe:getOpeningBalance')->everyMinute();
+        $schedule -> command('moneySafe:getOpeningBalance')->everyMinute();
 //         $schedule->command('inspire') ->hourly();
     }
 
